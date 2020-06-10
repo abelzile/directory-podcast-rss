3801c96e6abc08f3bd8f6d4e2b9842a918b1921a
Andre Belzile <andre.belzile@gmail.com>
2020-06-09 19:57:32 -0600

    RssController cleanup.

--- Controllers/RssController.cs
+++ Controllers/RssController.cs
@@ -1,8 +1,8 @@
+using System.IO;
 using System.Xml.Serialization;
+using DirectoryPodcastRss.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using IO = System.IO;
-using Model = DirectoryPodcastRss.Models;
 
 namespace DirectoryPodcastRss.Controllers
 {
@@ -18,11 +18,10 @@ namespace DirectoryPodcastRss.Controllers
         public IActionResult Index([FromServices] Options options)
         {
             var workingDirName = HashUtility.ComputeFileMd5Hash(options.ImageFilePath);
-            var workingPath = IO.Path.Combine(_env.WebRootPath, workingDirName);
-            var coverPath = IO.Path.Combine(workingPath, "cover.jpg");
+            var workingPath = Path.Combine(_env.WebRootPath, workingDirName);
+            var coverPath = Path.Combine(workingPath, "cover.jpg");
             var serverPath = $"{Request.Scheme}://{Request.Host.Value}/{workingDirName}";
-            var feedGenerator = new FeedGenerator(workingPath, coverPath, serverPath);
-            var rss = feedGenerator.Generate();
+            var rss = new FeedGenerator(workingPath, coverPath, serverPath).Generate();
 
             return new ContentResult
             {
@@ -32,17 +31,17 @@ namespace DirectoryPodcastRss.Controllers
             };
         }
 
-        private static string SerializeRss(Model.Rss rss)
+        private static string SerializeRss(Rss rss)
         {
             var ns = new XmlSerializerNamespaces();
 
-            foreach (var item in Model.Rss.XmlNamespaces)
+            foreach (var item in Rss.XmlNamespaces)
             {
                 ns.Add(item.Key, item.Value);
             }
 
             var serializer = new XmlSerializer(rss.GetType());
-            
+
             using (var writer = new Utf8StringWriter())
             {
                 serializer.Serialize(writer, rss, ns);
